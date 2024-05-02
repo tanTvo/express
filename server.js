f4790cf4a8088dd107d3e8f80de591b44ba2550e
Tan <tan10tvo@gmail.com>
2024-05-02 14:40:53 -0600

    f

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const postRoutes = require('./routes/postRoutes')
-const PORT = process.env.PORT || 3000
+const postRoutes = require('./routes/postRoutes');
+const ejs = require('ejs');
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use('/api/posts', postRoutes);
 
+app.set('view engine', 'ejs'); 
+app.set('views', __dirname + '/views'); 
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).json({message: ' server error'})
-})
-
-
-app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.status(500).render('error'); 
 });
 
+app.get('/', (req, res) => {
+    res.render('index'); 
+});
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
