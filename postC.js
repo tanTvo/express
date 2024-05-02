cec993467aa1fc20626967b660ba7bac5308cf44
Tan <tan10tvo@gmail.com>
2024-05-02 13:37:04 -0600

    update/delete

--- postC.js
+++ postC.js
@@ -14,5 +14,28 @@ exports.createPost = (req, res) => {
 };
 
 exports.deletePost = (req, res) => {
-    
+    const postId = req.params.id;
+    const index = post.findIndex(post => post.id === parseInt(postId));
+    if (index !== -1) {
+        posts.splice(index, 1);
+         res.status(204).send();
+    }else{
+        res.status(404).json({message: 'Not found'})
+    }
+   
+};
+
+exports.updatePost = (req, res) => {
+    const postId = req.params.id;
+    const {title, content, author}= req.body;
+    const postToUpdate = posts.find(post => post.id ===parseInt(postId));
+    if(postToUpdate){
+        postToUpdate.title = title || postToUpdate.title;
+        postToUpdate.content = content || postToUpdate.content;
+        postToUpdate.author = author || postToUpdate.author;
+        res.json(postToUpdate);
+    }else{
+        res.status(404).json({message: 'Not found'});
+    }
 }
+
